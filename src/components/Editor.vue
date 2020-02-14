a1b91a8d8b7ea0e4d998f86154a59dea9851e079
Delton Ding <dsh0416@gmail.com>
2020-02-14 19:59:37 +0900

    Improve function of editor

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -3,9 +3,10 @@
   .canvas(@dragover="dragover", draggable)
     .places(v-for="place in net.places")
       .place(
+        :key="place.label",
         @dragstart="dragstart($event, place.label, 'place')",
         @dragend="dragend",
-        @click="togglePanel",
+        @click="togglePanel(place.label, 'place')",
         :draggable="!readonly")
         place(
           draggable="true",
@@ -16,9 +17,10 @@
         )
     .transitions(v-for="transition in net.transitions")
       .transition(
+        :key="transition.label",
         @dragstart="dragstart($event, transition.label, 'transition')",
         @dragend="dragend",
-        @click="togglePanel",
+        @click="togglePanel(transition.label, 'transition')",
         :draggable="!readonly")
         p-transition(
           draggable="true",
@@ -26,6 +28,11 @@
           :x="transition.x"
           :y="transition.y"
         )
+    .buttons(v-if="!readonly")
+      button(@click="newPlace")
+        | Add New Place
+      button(@click="newTransition")
+        | Add New Transition
     .lines(draggable="false")
       svg.line(v-for="line in lines")
         line(
@@ -36,7 +43,78 @@
           :y2="line.y2",
           style="stroke:rgb(0,0,0);stroke-width:2")
   transition(name="fade")
-    .panel(v-if="showPanel")
+    .panel(v-if="selectLabel.length > 0")
+      .place-panel(v-if="selectLabelType === 'place'")
+        form
+          label.label(for="label")
+            | Label
+          br
+          input.input#label(type="text", :value="panelItem.label", disabled)
+          br
+          label.label(for="name")
+            | Name
+          br
+          input.input#name(type="text", v-model="panelItem.name", :disabled="readonly")
+          br
+          button(v-if="!readonly && \
+                        panelItem.label !== net.start_place && \
+                        panelItem.label !== net.end_place",
+                @click.prevent="deletePlace(panelItem.label)")
+            | Delete Place
+      .transition-panel(v-if="selectLabelType === 'transition'")
+        form
+          label.label(for="label")
+            | Label
+          br
+          input.input#label(type="text", :value="panelItem.label", disabled)
+          br
+          label.label(for="name")
+            | Name
+          br
+          input.input#name(type="text", v-model="panelItem.name", :disabled="readonly")
+          br
+          label.label(for="consume")
+            | Consume
+          ul.list
+            li(v-for="item in panelItem.consume")
+              span
+                | {{ item }}
+              button.btn-span(
+                v-if="!readonly",
+                @click.prevent="deleteConsume(panelItem.label, item)",
+              )
+                | DELETE
+            li
+              select(v-model="candidateLabel")
+                option(v-for="place in net.places", :key="place.label")
+                  | {{ place.label }}
+              button.btn-span(
+                v-if="!readonly",
+                @click.prevent="addConsume(panelItem.label, candidateLabel)")
+                | ADD
+          br
+          label.label(for="produce")
+            | Produce
+          ul.list
+            li(v-for="item in panelItem.produce")
+              span
+                | {{ item.label }}
+              button.btn-span(
+                v-if="!readonly",
+                @click.prevent="deleteProduce(panelItem.label, item.label)",
+              )
+                | DELETE
+            li
+              select(v-model="candidateLabel2")
+                option(v-for="place in net.places", :key="place.label")
+                  | {{ place.label }}
+              button.btn-span(
+                v-if="!readonly",
+                @click.prevent="addProduce(panelItem.label, candidateLabel2)")
+                | ADD
+          br
+          button(v-if="!readonly", @click.prevent="deleteTransition(panelItem.label)")
+            | Delete Transition
 </template>
 
 <script lang="ts">
@@ -79,22 +157,32 @@ export default class HelloWorld extends Vue {
     transitions: [],
   };
 
-  private showPanel = false;
+  private candidateLabel = 'start';
+
+  private candidateLabel2 = 'start';
 
   private dragging = '';
 
-  private draggingType = 'place'
+  private draggingType = 'place';
+
+  private selectLabel = '';
+
+  private selectLabelType = 'place';
 
   private mounted() {
     this.net = JSON.parse(JSON.stringify(this.rawData)); // Deep Copy
   }
 
-  private togglePanel() {
-    this.showPanel = !this.showPanel;
+  private togglePanel(label: string, type: string) {
+    this.selectLabelType = type;
+    if (label === this.selectLabel) {
+      this.selectLabel = '';
+    } else {
+      this.selectLabel = label;
+    }
   }
 
   private getPlaceType(label: string) {
-    console.log(label, this.net.start_place);
     if (label === this.net.start_place) return 'start';
     if (label === this.net.end_place) return 'end';
     return 'normal';
@@ -106,24 +194,145 @@ export default class HelloWorld extends Vue {
       trasition.consume.forEach((label) => {
         const c = this.net.places.filter((place) => place.label === label)[0];
         res.push({
-          x1: c.x + 25,
-          y1: c.y + 25,
-          x2: trasition.x + 50,
-          y2: trasition.y + 25,
+          x1: c.x + 25 + 5,
+          y1: c.y + 25 + 5,
+          x2: trasition.x + 50 + 5,
+          y2: trasition.y + 25 + 5,
         });
       });
 
       trasition.produce.forEach((produce) => {
         const p = this.net.places.filter((place) => place.label === produce.label)[0];
         res.push({
-          x1: trasition.x + 50,
-          y1: trasition.y + 25,
-          x2: p.x + 25,
-          y2: p.y + 25,
+          x1: trasition.x + 50 - 5,
+          y1: trasition.y + 25 - 5,
+          x2: p.x + 25 - 5,
+          y2: p.y + 25 - 5,
         });
       });
     });
     return res;
+  }
+
+  private get panelItem() {
+    if (this.selectLabelType === 'place') {
+      return this.net.places.filter((place) => place.label === this.selectLabel)[0];
+    }
+    if (this.selectLabelType === 'transition') {
+      return this.net.transitions.filter((transition) => transition.label === this.selectLabel)[0];
+    }
+    return null;
+  }
+
+  private newPlace() {
+    // eslint-disable-next-line no-alert
+    const label = prompt('Please Enter Label For New Place');
+
+    if (label === '' || label === null) {
+      // eslint-disable-next-line no-alert
+      alert('Label Can\'t be Empty.');
+      return;
+    }
+
+    if (this.net.places.filter((place) => place.label === label).length > 0) {
+      // eslint-disable-next-line no-alert
+      alert('Label Existed.');
+      return;
+    }
+
+    this.net.places.push({
+      label,
+      name: label,
+      x: 0,
+      y: 0,
+    });
+  }
+
+  private deletePlace(label: string) {
+    // Delete All Related Links
+    this.net.transitions.forEach((transition) => {
+      if (transition.consume.indexOf(label) > -1) {
+        transition.consume.splice(transition.consume.indexOf(label), 1);
+      }
+
+      const filtered = transition.produce.filter((produce) => produce.label === label);
+
+      if (filtered.length > 0) {
+        transition.produce.splice(transition.produce.indexOf(filtered[0]), 1);
+      }
+    });
+
+    // Delete the Place
+    this.selectLabel = '';
+    const place = this.net.places.filter((p) => p.label === label)[0];
+    this.net.places.splice(this.net.places.indexOf(place), 1);
+  }
+
+  private deleteTransition(label: string) {
+    this.selectLabel = '';
+    const transition = this.net.transitions.filter((t) => t.label === label)[0];
+    this.net.transitions.splice(this.net.transitions.indexOf(transition), 1);
+  }
+
+  private newTransition() {
+    // eslint-disable-next-line no-alert
+    const label = prompt('Please Enter Label For New Transition');
+
+    if (label === '' || label === null) {
+      // eslint-disable-next-line no-alert
+      alert('Label Can\'t be Empty.');
+      return;
+    }
+
+    if (this.net.transitions.filter((transition) => transition.label === label).length > 0) {
+      // eslint-disable-next-line no-alert
+      alert('Label Existed.');
+      return;
+    }
+
+    this.net.transitions.push({
+      label,
+      name: label,
+      consume: [],
+      produce: [],
+      x: 0,
+      y: 0,
+    });
+  }
+
+  private addConsume(transitionLabel: string, placeLabel: string) {
+    const transition = this.net.transitions.filter((t) => t.label === transitionLabel)[0];
+    if (transition.consume.filter((c) => c === placeLabel).length > 0) {
+      // eslint-disable-next-line no-alert
+      alert('Already Connected');
+      return;
+    }
+    transition.consume.push(placeLabel);
+  }
+
+  private addProduce(transitionLabel: string, placeLabel: string) {
+    const transition = this.net.transitions.filter((t) => t.label === transitionLabel)[0];
+    if (transition.produce.filter((c) => c.label === placeLabel).length > 0) {
+      // eslint-disable-next-line no-alert
+      alert('Already Connected');
+      return;
+    }
+    transition.produce.push({
+      label: placeLabel,
+      guard: null,
+    });
+  }
+
+  private deleteConsume(transitionLabel: string, placeLabel: string) {
+    const transition = this.net.transitions.filter((t) => t.label === transitionLabel)[0];
+    const toDelete = transition.consume.filter((c) => c === placeLabel)[0];
+    transition.consume.splice(transition.consume.indexOf(toDelete), 1);
+  }
+
+  private deleteProduce(transitionLabel: string, placeLabel: string) {
+    const transition = this.net.transitions.filter((t) => t.label === transitionLabel)[0];
+    const toDelete = transition.produce.filter((c) => c.label === placeLabel)[0];
+    transition.produce.splice(transition.produce.indexOf(toDelete), 1);
   }
 
   // eslint-disable-next-line class-methods-use-this
@@ -132,11 +341,7 @@ export default class HelloWorld extends Vue {
     const editor = this.$refs.editor as HTMLElement;
 
     const resultX = e.clientX - editor.offsetLeft;
-    let resultY = e.clientY - editor.offsetTop;
-
-    if (window.navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
-      resultY += 25;
-    }
+    const resultY = e.clientY - editor.offsetTop;
 
     if (this.draggingType === 'place') {
       const element = this.net.places.filter((value) => value.label === this.dragging)[0];
@@ -175,6 +380,7 @@ export default class HelloWorld extends Vue {
   height 100%
   display flex
   background #cfd4e0
+  font-family 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif
 
 .canvas
   flex 1
@@ -198,10 +404,29 @@ export default class HelloWorld extends Vue {
   background #617198
   border-radius 4px 0 0 4px
   box-shadow -2px 0px 2px #aaa
+  padding 20px
+
+.label
+  color #fff
+
+.list
+  color #fff
+
+.input
+  margin-bottom 10px
 
 .fade-enter-active, .fade-leave-active
   transition opacity .1s
 
 .fade-enter, .fade-leave-to
   opacity 0
+
+.buttons
+  position absolute
+  bottom 0
+  z-index 4
+
+.btn-span
+  margin 0 10px
+  cursor pointer
 </style>
